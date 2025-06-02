ee13d3b48503193da80e0f350c716670fdb7816f
Mahdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-06-02 15:48:22 +0330

    main page footer added

--- components/navigation/Footer.tsx
+++ components/navigation/Footer.tsx
@@ -1,3 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/public/logo.svg";
+import { footerLinks } from "@/constants/constants";
+
 export default function Footer() {
-  return <div>footer</div>;
+  return (
+    <footer className="grid sm:grid-cols-3 xl:grid-cols-4 gap-10 w-full text-gray-700 mx-20 pt-10 mb-10 mt-15 text-black-100 border-t border-gray-300 ">
+      <div className="">
+        <Image
+          src={logo}
+          alt="logo"
+          width={118}
+          height={18}
+          className="mb-6 mt-2"
+        />
+        <div className="text-gray-700 ml-3">
+          <p>Carhub 2025</p>
+          <p>All Rights Reserved &copy;</p>
+        </div>
+      </div>
+
+      {footerLinks.map((footerLink) => (
+        <div>
+          <h3 className="text-black font-bold text-lg">{footerLink.title}</h3>
+          <ul className="mt-3">
+            {footerLink.links.map((link) => (
+              <li>
+                <Link href={link.url}>{link.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </footer>
+  );
 }
