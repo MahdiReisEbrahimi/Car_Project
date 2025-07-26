7996d9afb2c638e1c3c720fb10a3c4fadc05042a
Mahdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-07-26 19:09:02 +0330

    new slice added to store

--- store/index.ts
+++ store/index.ts
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { carsApi } from "./API/carsApi";
+import carFilterReducer from './slices/CarFilterReducer'
 
 const store = configureStore({
   reducer: {
     [carsApi.reducerPath]: carsApi.reducer,
+    carFilter : carFilterReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(carsApi.middleware),
 });
 
 export default store;
+export type RootState = ReturnType<typeof store.getState>;
+
