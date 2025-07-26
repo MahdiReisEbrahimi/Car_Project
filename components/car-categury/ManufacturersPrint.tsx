acf8963829bc9fc44cada5c7f1d5c5146c27bde2
Mahdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-07-26 19:07:13 +0330

    adding Manufacturers to store before printing

--- components/car-categury/ManufacturersPrint.tsx
+++ components/car-categury/ManufacturersPrint.tsx
@@ -1,25 +1,35 @@
 "use client";
-import {
-  useGetAllManufacturersQuery,
-} from "@/store/API/carsApi";
+import { useGetAllManufacturersQuery } from "@/store/API/carsApi";
+import CarsPrint from "./CarsPrint";
+import { useDispatch } from "react-redux";
+import { setManufacturers } from "@/store/slices/CarFilterReducer";
+import { useEffect } from "react";
 
 export default function ManufacturersPrint() {
-  const { data: manufacturers, isLoading: isManufacturersLoading } = useGetAllManufacturersQuery();
+  const { data: manufacturers, isLoading: isManufacturersLoading } =
+    useGetAllManufacturersQuery();
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const tempManufac = manufacturers?.filter(
+      (manufacturer) => manufacturer.Mfr_CommonName !== null
+    );
+    if (tempManufac && !isManufacturersLoading)
+      dispatch(setManufacturers(tempManufac));
+  }, [isManufacturersLoading, manufacturers, dispatch]);
 
   return (
-    <div className="grid grid-cols-3 gap-2 border rounded-2xl p-4">
+    <div className="grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2  gap-2 border rounded-2xl p-4">
       {isManufacturersLoading ? (
         <p>Loading...</p>
       ) : (
         manufacturers?.map(
           (manufacturer) =>
             manufacturer.Mfr_CommonName !== null && (
-              <h2
+              <CarsPrint
                 key={manufacturer.Mfr_ID}
-                className="border-b rounded-2xl p-2 text-sm bg-gray-400 cursor-pointer hover:bg-gray-300 text-center "
-              >
-                {manufacturer.Mfr_CommonName}
-              </h2>
+                manufacturer={manufacturer}
+              />
             )
         )
       )}
