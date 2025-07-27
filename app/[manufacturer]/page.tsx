6a72d774579ab1afaa1addc4b4f91f94ea8ca9cd
Mahdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-07-28 02:10:39 +0330

    some UI changes on manufacturer detail page

--- app/[manufacturer]/page.tsx
+++ app/[manufacturer]/page.tsx
@@ -3,6 +3,7 @@ import { useParams } from "next/navigation";
 import { useFindManufacturerName } from "@/hooks/useFindManufacturerName";
 import { useGetMakesByManufacturerQuery } from "@/store/API/carsApi";
 import { Makes } from "@/types";
+import MakesPrint from "./MakesPrint";
 
 export default function ManufacturerDetail() {
   const params = useParams();
@@ -18,7 +19,7 @@ export default function ManufacturerDetail() {
   });
 
   // delete the repeated data:
-  const uniqeData : Makes[] = [];
+  const uniqeData: Makes[] = [];
   const seen = new Set();
 
   data?.Results?.forEach((item) => {
@@ -32,13 +33,30 @@ export default function ManufacturerDetail() {
   if (findNameEror) return <div>This Manufacturer Id does not exist!</div>;
 
   return (
-    <div>
-      {uniqeData?.map((make) => (
-        <div key={make.Make_ID}>
-          {make.Make_Name} : {make.Make_ID}
-        </div>
-      ))}
-      <div>this is manufacturer page : {params.manufacturer}</div>
+    <div className="px-4 py-6">
+      <div className="mb-8 text-center">
+        <h1 className="text-3xl sm:text-4xl font-bold text-gray-800 mb-2">
+          {manufacturerName}
+        </h1>
+        <p className="text-sm sm:text-base text-gray-600 max-w-xl mx-auto">
+          {manufacturerName} is one of the{" "}
+          <span className="font-semibold text-pink-600">
+            biggest car manufacturers
+          </span>{" "}
+          in the world, known for innovation and performance across various
+          models.
+        </p>
+        <p className="mt-4 text-base font-medium text-gray-700">
+          🚗 Cars Available:{" "}
+          <span className="text-pink-600">{uniqeData.length}</span>
+        </p>
+      </div>
+      {/*Available Cars grid*/}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 justify-items-center">
+        {uniqeData?.map((make) => (
+          <MakesPrint key={make.Make_ID} make={make} />
+        ))}
+      </div>
     </div>
   );
 }
