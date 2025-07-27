f8075a584cb3661dd4f3b6ed1d5a1184ef6b1238
Mahdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-07-27 18:29:13 +0330

    add a new hook to take the name of a manufacturer id and if there was no data on redux => take data from server + store data in redux

---
+++ hooks/useFindManufacturerName.tsx
@@ -0,0 +1,42 @@
+import { useGetAllManufacturersQuery } from "@/store/API/carsApi";
+import { setManufacturers } from "@/store/slices/CarFilterReducer";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useMemo } from "react";
+import { RootState } from "@/store";
+
+export function useFindManufacturerName(manufacturerId: number) {
+  const dispatch = useDispatch();
+
+  const manufacturers = useSelector(
+    (state: RootState) => state.carFilter.wholeManufacturers
+  );
+
+  const {
+    data: fetchedManufacturers,
+    isLoading,
+    isSuccess,
+    error,
+  } = useGetAllManufacturersQuery(undefined, {
+    skip: manufacturers !== null && manufacturers.length > 0, // Just when we have no data in store => take data.
+  });
+
+  // dispatch data into redux (if there was no data...)
+  useEffect(() => {
+    if (!manufacturers || manufacturers.length === 0) {
+      if (isSuccess && fetchedManufacturers) {
+        const validManufacturers = fetchedManufacturers.filter(
+          (m) => m.Mfr_CommonName !== null
+        );
+        dispatch(setManufacturers(validManufacturers));
+      }
+    }
+  }, [isSuccess, fetchedManufacturers, manufacturers, dispatch]);
+
+  // lets find manufacturer name
+  const manufacturerName = useMemo(() => {
+    return manufacturers?.find((m) => m.Mfr_ID === manufacturerId)
+      ?.Mfr_CommonName;
+  }, [manufacturers, manufacturerId]);
+
+  return { manufacturerName, isLoading, error };
+}
