2a31829309e126a58cee9783f63aaa7c7787a7b7
Mahdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-08-03 14:18:35 +0330

    added Get and Print components for manufacturers

---
+++ components/car-categury/options/manufacturers/GetManufacturers.tsx
@@ -0,0 +1,54 @@
+"use client";
+import type { RootState } from "@/store/index";
+import { useGetAllManufacturersQuery } from "@/store/API/carsApi";
+
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setManufacturers } from "@/store/slices/CarFilterReducer";
+import PrintManufacturers from "./PrintManufacturer";
+
+export default function GetManufacturers() {
+  const { data: manufacturers, isLoading: isManufacturersLoading } =
+    useGetAllManufacturersQuery();
+
+  const dispatch = useDispatch();
+  const storedManufacturers = useSelector(
+    (state: RootState) => state.carFilter.filteredManufacturers
+  );
+
+  const wholeManufacturers = useSelector(
+    (state: RootState) => state.carFilter.wholeManufacturers
+  );
+
+  useEffect(() => {
+    const tempManufac = manufacturers?.filter(
+      (manufacturer) => manufacturer.Mfr_CommonName !== null
+    );
+
+    if (
+      tempManufac &&
+      !isManufacturersLoading &&
+      (!wholeManufacturers || wholeManufacturers.length === 0)
+    ) {
+      dispatch(setManufacturers(tempManufac));
+    }
+  }, [manufacturers, isManufacturersLoading, storedManufacturers, dispatch]);
+
+  return (
+    <div className="grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2  gap-2 border rounded-2xl p-4">
+      {isManufacturersLoading ? (
+        <p>Loading...</p>
+      ) : (
+        storedManufacturers?.map(
+          (manufacturer) =>
+            manufacturer.Mfr_CommonName !== null && (
+              <PrintManufacturers
+                key={manufacturer.Mfr_ID}
+                manufacturer={manufacturer}
+              />
+            )
+        )
+      )}
+    </div>
+  );
+}
