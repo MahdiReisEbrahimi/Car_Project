84bc6a75f7e27bd7943fce45f13edaa288baa1aa
Mahdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-08-08 00:21:47 +0330

    data base has no images. so i added some img for better view.

--- components/car-categury/options/manufacturers/PrintManufacturer.tsx
+++ components/car-categury/options/manufacturers/PrintManufacturer.tsx
@@ -1,6 +1,18 @@
 import { Manufacturer } from "@/types";
 import { Factory, Globe2, Hash } from "lucide-react";
 import Link from "next/link";
+import car1 from "@/public/cars/car1.jpeg";
+import car2 from "@/public/cars/car2.jpg";
+import car3 from "@/public/cars/car3.avif";
+import car4 from "@/public/cars/car4.avif";
+import car5 from "@/public/cars/car5.jpg";
+import car6 from "@/public/cars/car6.webp";
+import car7 from "@/public/cars/car7.webp";
+import car8 from "@/public/cars/car8.avif";
+import car9 from "@/public/cars/car9.jpeg";
+import car10 from "@/public/cars/car10.avif";
+import car11 from "@/public/cars/car11.webp";
+import Image from "next/image";
 
 interface PrintManufacturersType {
   manufacturer: Manufacturer;
@@ -9,9 +21,33 @@ interface PrintManufacturersType {
 export default function PrintManufacturers({
   manufacturer,
 }: PrintManufacturersType) {
+  const carImages = [
+    car1,
+    car2,
+    car3,
+    car4,
+    car5,
+    car6,
+    car7,
+    car8,
+    car9,
+    car10,
+    car11,
+  ];
+  const randomIndex = Math.floor(Math.random() * carImages.length);
+  const randomCar = carImages[randomIndex];
   return (
     <Link href={`/manufacturers/${manufacturer.Mfr_ID}`}>
-      <div className="rounded-2xl cursor-pointer bg-white shadow-lg border h-45 border-gray-200 hover:shadow-xl hover:scale-[1.015] transition-all duration-300 p-5 flex flex-col gap-3">
+      <div className="rounded-2xl cursor-pointer bg-white shadow-lg border h-85 border-gray-200 hover:shadow-xl hover:scale-[1.015] transition-all duration-300 p-5 flex flex-col gap-3">
+        <div className="relative w-full h-40">
+          <Image
+            src={randomCar}
+            fill
+            alt="Random car"
+            className="object-cover rounded-xl"
+          />
+        </div>
+
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-2 text-gray-800">
             <Factory className="w-5 h-5 text-indigo-600" />
