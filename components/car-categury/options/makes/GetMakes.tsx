bd955862fe0fbc7a5da8ad82269e40366e527363
Mahdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-08-08 14:00:44 +0330

    some changes on pagination of makes

--- components/car-categury/options/makes/GetMakes.tsx
+++ components/car-categury/options/makes/GetMakes.tsx
@@ -2,18 +2,22 @@
 
 import PrintMakes from "./PrintMakes";
 import { useGetAndSaveMakes } from "@/hooks/useGetAndSaveMakes";
-import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setCurrentMakesPage } from "@/store/slices/CarFilterReducer";
+import { Make } from "@/types";
 
 export default function GetMakes() {
-  const { error, makes, isLoading } = useGetAndSaveMakes();
-  const [page, setPage] = useState(1); // Track current page
-  const itemsPerPage = 52;
+  const { isLoading } = useGetAndSaveMakes();
+  const dispatch = useDispatch();
 
-  // Split into chunks
+  const makes = useSelector((state: any) => state.carFilter.filteredMakes);
+  const page = useSelector((state: any) => state.carFilter.currentMakesPage);
+  const itemsPerPage = 20;
+
   const totalPages = Math.ceil((makes?.length || 0) / itemsPerPage);
   const startIndex = (page - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
-  const currentItems = makes?.slice(startIndex, endIndex);
+  const currentItems: Make[] = makes?.slice(startIndex, endIndex);
 
   return (
     <div>
@@ -33,61 +37,36 @@ export default function GetMakes() {
 
       {/* Pagination */}
       <div className="flex m-auto rounded-lg p-3 max-w-fit overflow-auto justify-center gap-3 mt-6 bg-black/40 backdrop-blur-md shadow-lg">
-        {/* first page */}
         {page !== 1 && (
           <button
-            disabled={page === 1}
-            onClick={() => setPage(1)}
-            className={`px-4 cursor-pointer py-1.5 rounded-full text-sm font-medium transition-all duration-200 
-      ${
-        page === 1
-          ? "bg-gray-700/50 text-gray-400 cursor-not-allowed"
-          : "bg-white/10 text-white hover:bg-white/20 hover:scale-105"
-      }`}
+            onClick={() => dispatch(setCurrentMakesPage(1))}
+            className="px-4 py-1.5 rounded-full text-sm font-medium bg-white/10 text-white hover:bg-white/20 hover:scale-105"
           >
             First
           </button>
         )}
-
-        {/* Prev Button */}
         {page !== 1 && (
           <button
-            disabled={page === 1}
-            onClick={() => setPage((p) => p - 1)}
-            className={`px-4 cursor-pointer py-1.5 rounded-full text-sm font-medium transition-all duration-200 
-      ${
-        page === 1
-          ? "bg-gray-700/50 text-gray-400 cursor-not-allowed"
-          : "bg-white/10 text-white hover:bg-white/20 hover:scale-105"
-      }`}
+            onClick={() => dispatch(setCurrentMakesPage(page - 1))}
+            className="px-4 py-1.5 rounded-full text-sm font-medium bg-white/10 text-white hover:bg-white/20 hover:scale-105"
           >
             Prev
           </button>
         )}
-
-        {/* Current Page */}
         <span className="px-4 py-1.5 rounded-full text-sm font-bold bg-gradient-to-r from-gray-800 to-gray-500 text-white shadow-md">
           {page}
         </span>
-
-        {/* Next Button */}
         <button
           disabled={page === totalPages}
-          onClick={() => setPage((p) => p + 1)}
-          className={`px-4 cursor-pointer py-1.5 rounded-full text-sm font-medium transition-all duration-200 
-      ${
-        page === totalPages
-          ? "bg-gray-700/50 text-gray-400 cursor-not-allowed"
-          : "bg-white/10 text-white hover:bg-white/20 hover:scale-105"
-      }`}
+          onClick={() => dispatch(setCurrentMakesPage(page + 1))}
+          className="px-4 py-1.5 rounded-full text-sm font-medium bg-white/10 text-white hover:bg-white/20 hover:scale-105 disabled:opacity-50"
         >
           Next
         </button>
-        {/* Last Page Button */}
         {page !== totalPages && (
           <button
-            onClick={() => setPage(totalPages)}
-            className="px-4 cursor-pointer py-1.5 rounded-full text-sm font-medium bg-white/10 text-white hover:bg-white/20 hover:scale-105 transition-all duration-200"
+            onClick={() => dispatch(setCurrentMakesPage(totalPages))}
+            className="px-4 py-1.5 rounded-full text-sm font-medium bg-white/10 text-white hover:bg-white/20 hover:scale-105"
           >
             Last ({totalPages})
           </button>
