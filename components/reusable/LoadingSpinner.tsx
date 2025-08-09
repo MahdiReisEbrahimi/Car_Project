9f6cdba5a31f637f6d103a184966cebe892a1f7d
Mahdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-08-09 19:38:59 +0330

    some style change on Loading spinner

--- components/reusable/LoadingSpinner.tsx
+++ components/reusable/LoadingSpinner.tsx
@@ -2,14 +2,17 @@ interface LoadingProps {
   message?: string;
 }
 
-export default function LoadingSpinner({ message = "Loading..." }: LoadingProps) {
+export default function LoadingSpinner({
+  message = "Loading...",
+}: LoadingProps) {
   return (
-    <div className="flex items-center justify-center p-6 bg-white shadow-lg rounded-lg min-w-[220px] min-h-[80px] space-x-4 mt-2">
-      {/* متن پیام */}
-      <span className="text-gray-800 font-semibold text-lg select-none">{message}</span>
+    <div className="m-auto flex items-center justify-center p-6 bg-gray-400 shadow-lg rounded-lg min-w-[220px] min-h-[80px] space-x-4 mt-2">
+      <span className="text-black font-semibold text-lg select-none">
+        {message}
+      </span>
 
       <svg
-        className="animate-spin h-5 w-5 text-pink-600"
+        className="animate-spin h-5 w-5 text-black"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
